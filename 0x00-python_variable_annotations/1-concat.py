eaac39eb84d77c11b3b2814a6473b04785984a7a
MustaphaRam <mustapha.mad.75@gmail.com>
2024-01-11 13:41:44 +0100

    0x00-python_variable_annotations

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,7 +4,7 @@
 """
 
 
-def add(str1: str, str2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     '''takes a string str1 and a string str2
     as arguments and returns a concatenated string .'''
     return str1 + str2
