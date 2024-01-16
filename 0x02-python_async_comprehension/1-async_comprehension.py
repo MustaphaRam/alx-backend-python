776e5b1ce49b4ed4a0dbc1c46cbcd6c8e4caa96d
MustaphaRam <mustapha.mad.75@gmail.com>
2024-01-16 13:42:34 +0100

    0x02-python_async_comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Write a coroutine called async_generator that takes no arguments.
+The coroutine will loop 10 times, each time asynchronously wait 1 second,
+then yield a random number between 0 and 10. Use the random module.
+"""
+from typing import List
+from importlib import import_module as using
+
+async_generator = using('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    '''Creates a list of 10 numbers from a 10-number generator.
+    '''
+    return [num async for num in async_generator()]
